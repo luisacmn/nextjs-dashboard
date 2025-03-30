812024589d40f8e76a3e89407269e38d9698032b
Luisa Novaes <luisanovaes.arq@gmail.com>
2025-03-30 14:51:29 -0300

    R01- Fetch data  + Dynamic rendering + Streaming

---
+++ app/dashboard/(overview)/page.tsx
@@ -0,0 +1,32 @@
+import CardWrapper from "@/app/dashboard/cards";
+import { Card } from "@/app/dashboard/cards";
+import RevenueChart from "@/app/dashboard/revenue-chart";
+import { lusitana } from "@/app/ui/fonts";
+import { Suspense } from "react";
+import {
+  RevenueChartSkeleton,
+  LatestInvoicesSkeleton,
+  CardsSkeleton,
+} from "@/app/ui/skeletons";
+import LatestInvoices from "../latest-invoices";
+
+export default async function Page() {
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <CardWrapper />
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
+      </div>
+    </main>
+  );
+}
